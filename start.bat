c4ff46df7fa6507ae3bc02a5dd7125067cea91cd
Jean-Philippe Encausse <Jp@encausse.net>
2017-09-10 01:09:04 +0200

    Update start.bat with new template

--- start.bat
+++ start.bat
@@ -1,4 +1,35 @@
+@echo off
+
+:: Project paths
+IF "%BOT_ROOT%" == "" GOTO HELPER
+SET FRAMEWORK_ROOT=%~dp0
+
+:: Project configs
+SET NODE_RED=%FRAMEWORK_ROOT%\node_modules\node-red\red.js
+SET CONFIG_PATH=%BOT_ROOT%\conf\config.js
+
+:: Project parameters
+IF "%NODE_ENV%" == "" SET NODE_ENV=dev
+IF "%PORT%" == ""     SET PORT=1880
+IF "%HOST%" == ""     SET HOST=http://127.0.0.1
+:: SET CREDENTIAL_SECRET=a-secret-key
+
+:: Required to fix SSL issues
 SET NODE_TLS_REJECT_UNAUTHORIZED=0
-SET BOTBUILDER_CFG={cwd}\data\config.json
-SET PORT=1880
-node node-red\node-red-current\red.js -s data\node-red-config.js
+
+:: MUST start in the project folder
+@echo Starting Node-RED
+CD %BOT_ROOT%
+node "%NODE_RED%" -s "%FRAMEWORK_ROOT%\conf\node-red-config.js"
+GOTO END
+
+:HELPER
+
+@echo This script require following variables:
+@echo - BOT_ROOT: the project directory
+@echo - NODE_ENV: the properties configuration (dev)
+@echo - PORT:     the server port (1880)
+@echo - HOST:     the external host URL
+@echo NodeJS v8 must be into the PATH
+
+:END

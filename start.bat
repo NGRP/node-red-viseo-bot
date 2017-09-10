d7b78e1e942d2fbc913b0e37ab5f2fd141704adb
Jean-Philippe Encausse <Jp@encausse.net>
2017-09-10 21:06:43 +0200

    Fix variables and bugs

--- start.bat
+++ start.bat
@@ -5,13 +5,14 @@ IF "%BOT_ROOT%" == "" GOTO HELPER
 SET FRAMEWORK_ROOT=%~dp0
 
 :: Project configs
-SET NODE_RED=%FRAMEWORK_ROOT%\node_modules\node-red\red.js
-SET CONFIG_PATH=%BOT_ROOT%\conf\config.js
+IF "%CONFIG_PATH%"      == "" SET CONFIG_PATH=%BOT_ROOT%\conf\config.js
+IF "%NODE_RED%"         == "" SET NODE_RED=%FRAMEWORK_ROOT%\node_modules\node-red\red.js
+IF "%NODE_RED_CONFIG%"  == "" SET NODE_RED_CONFIG=%FRAMEWORK_ROOT%\conf\node-red-config.js
 
 :: Project parameters
 IF "%NODE_ENV%" == "" SET NODE_ENV=dev
-IF "%PORT%" == ""     SET PORT=1880
-IF "%HOST%" == ""     SET HOST=http://127.0.0.1
+IF "%PORT%"     == "" SET PORT=1880
+IF "%HOST%"     == "" SET HOST=http://127.0.0.1
 :: SET CREDENTIAL_SECRET=a-secret-key
 
 :: Required to fix SSL issues
@@ -20,7 +21,7 @@ SET NODE_TLS_REJECT_UNAUTHORIZED=0
 :: MUST start in the project folder
 @echo Starting Node-RED
 CD %BOT_ROOT%
-node "%NODE_RED%" -s "%FRAMEWORK_ROOT%\conf\node-red-config.js"
+node "%NODE_RED%" -s "%NODE_RED_CONFIG%"
 GOTO END
 
 :HELPER

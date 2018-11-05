9e335e5ab23650ab2bde25f2367d568309cfa88b
Klervi <klervi.menoret@gmail.com>
2018-11-05 09:56:14 +0100

    SET httpAdminRoot

--- conf/node-red-config.js
+++ conf/node-red-config.js
@@ -89,7 +89,7 @@ module.exports = extend(settings, true, {
     // By default, the Node-RED UI is available at http://localhost:1880/
     // The following property can be used to specifiy a different root path.
     // If set to false, this is disabled.
-    //httpAdminRoot: '/admin',
+    httpAdminRoot:  process.env.PORT || '/',
 
     // Some nodes, such as HTTP In, can be used to listen for incoming http requests.
     // By default, these are served relative to '/'. The following property
